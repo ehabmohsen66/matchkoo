c8991c863f6ec5c609fd8ce1da52b3563db880e8
a3atef <ahmed.mousa.it@gmail.com>
2026-06-11 13:52:21 +0300

    UI: remove cta-note checkmarks line from landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -351,7 +351,6 @@ export default function Home() {
               <svg viewBox="0 0 20 20" fill="currentColor" width="16" height="16"><path fillRule="evenodd" d="M3 10a.75.75 0 01.75-.75h10.638L10.23 5.29a.75.75 0 111.04-1.08l5.5 5.25a.75.75 0 010 1.08l-5.5 5.25a.75.75 0 11-1.04-1.08l4.158-3.96H3.75A.75.75 0 013 10z" clipRule="evenodd" /></svg>
             </a>
           </div>
-          <div className="cta-note">✓ Free forever &nbsp;&nbsp; ✓ 188 leagues &nbsp;&nbsp; ✓ 2.5k community</div>
         </div>
       </section>
 
